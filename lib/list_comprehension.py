3f8b823445a197118504eaf374023d20fb53d7a7
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2024-04-02 20:34:53 -0700

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [number for number in num_list if number % 2 == 0]
+    return even_list
 
 def make_exclamation(sentence_list):
-    pass
+    exclamated_list = [sentence + "!" for sentence in sentence_list]
+    return exclamated_list
